e324edc543de9a8a71870e0fce86c186ff747838
Gabriel Fernandes <gabgui2001@gmail.com>
2023-01-21 02:20:02 -0300

    algoritimo de demanda de chamados
    Desenvolvimento do algoritimo de demanda de chamados na div chamados abertos

--- dashboard-GLPI/src/app/components/div-chamados/div-chamados.component.ts
+++ dashboard-GLPI/src/app/components/div-chamados/div-chamados.component.ts
@@ -7,11 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DivChamadosComponent implements OnInit{
 
-  chamados: number[] = [8843, 8843, 8843, 8843 , 8843, 8843, 8843, 8843, 8843 , 8843];
-  iconChamdo: string = "";
+  chamados: number[] = [];
+
+  buscaPeriodo: any[] = [];
+  iconSeta: string = "";
+  demanda: number | string = 0;
+
   constructor(){}
   ngOnInit() {
 
+    this.chamados = [8843, 8843, 8843, 8843 , 8843, 8843, 8843, 8843, 8843 , 8843];
+
+    this.buscaPeriodo =  [{
+      periodo: "novembro",
+      chamados: [333,222,111,555,666,776,776]
+    },
+    {
+      periodo: "dezembro",
+      chamados: [222,111,555,666,776,222,111,555,666,776,776]
+    }];
+
+
+    this.checkDemanda(this.buscaPeriodo[0].chamados.length, this.buscaPeriodo[1].chamados.length);
+  }
+
+  checkDemanda(Data1: number,Data2: number){
+    this.demanda = ((Data2-Data1)*100/Data2).toFixed(0);
+    if(Data1 > Data2){
+      this.iconSeta = "../../../assets/seta baixo.png";
+    }else{
+      this.iconSeta = "../../../assets/seta cima.png";
+    }
   }
 
   getChamado(){
